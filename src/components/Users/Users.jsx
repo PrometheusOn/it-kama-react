69aeef1acff6ea4223533239b8463cd2ae937ad2
PrometheusOn <ramazanfajzov@mail.ru>
2023-07-20 18:07:31 +0500

    #55 Пагинация, жизненные циклы(ComponentDidMount()\, ComponentDidUpdate())

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,47 @@ import axios from "axios";
 import userPhoto from "../../assets/images/user.png";
 
 class Users extends React.Component {
-	constructor(props) {
-		super(props);
-		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+			this.props.setUsers(response.data.items);
+			this.props.setTotalUsersCount(response.data.totalCount)
+		});
+	}
+
+	onPageChanged = (pageNumber) => {
+		this.props.setCurrentPage(pageNumber)
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
 			this.props.setUsers(response.data.items);
 		});
 	}
 
 	render() {
+
+		const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+		const pages = []
+		for (let i = 1; i <= pagesCount; i++) {
+			pages.push(i)
+		}
+
+		// const pagesCount = () => {
+		// 	let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+		// 	if( pagesCount > 10) {
+		// 		pagesCount = 10
+		// 	}
+		// 	return pagesCount
+		// }
+		// const pages = []
+		// for (let i = 1; i <= pagesCount(); i++) {
+		// 	pages.push(i)
+		// }
+
 		return (
 			<div className={classes.userList}>
+				<div className={ classes.pagination }>
+					{pages.map( p =>{
+						return <span className={this.props.currentPage === p && classes.selectedPage} onClick={(e)=>{ this.onPageChanged(p) }}>{ p }</span>
+					})}
+				</div>
 				{this.props.users.map(user => (
 					<div key={user.id} className={classes.user}>
 						<div className={classes.imageProfileBlock}>
